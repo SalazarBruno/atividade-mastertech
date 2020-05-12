75c3c6f6cf16492f3a12d27359141dbc683556b9
bruno <salazar.bruno@gmail.com>
2020-05-11 21:13:31 -0300

    Adicionado lancamento de ponto

---
+++ ponto/src/main/java/br/com/brunomagnum/ponto/TimeEntry.java
@@ -0,0 +1,48 @@
+package br.com.brunomagnum.ponto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class TimeEntry {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+  private String userId;
+  private String type;
+  private String date;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+}
---
+++ ponto/src/main/java/br/com/brunomagnum/ponto/TimeEntryRepository.java
@@ -0,0 +1,12 @@
+package br.com.brunomagnum.ponto;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.brunomagnum.ponto.TimeEntry;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface TimeEntryRepository extends CrudRepository<TimeEntry, Integer> {
+
+}
