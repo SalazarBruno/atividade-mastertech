ed110709fba416e29fc71332379581b6ef6357e0
Bruno M V M Salazar <salazar.bruno@gmail.com>
2020-07-05 22:34:17 -0300

    User Refactor

---
+++ ponto/src/main/java/br/com/brunomagnum/Ponto/models/UserModel.java
@@ -0,0 +1,74 @@
+package br.com.brunomagnum.Ponto.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+  
+    @NotNull
+    private String name;
+    
+    @NotNull
+    @CPF
+    private String cpf;
+    
+    @NotNull
+    @Email
+    private String email;
+    
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+  
+    
+}
--- ponto/src/main/java/br/com/brunomagnum/Ponto/repositories/UserRepository.java
+++ ponto/src/main/java/br/com/brunomagnum/Ponto/repositories/UserRepository.java
@@ -1,12 +1,13 @@
-package br.com.brunomagnum.ponto;
+package br.com.brunomagnum.Ponto.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import br.com.brunomagnum.ponto.User;
+import br.com.brunomagnum.Ponto.models.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
-
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
